b90dd41d9b5cb210d00058134ccba42c9c77dc33
Tony <yc980@georgetown.edu>
2022-08-23 17:17:58 -0400

    initial-commit

---
+++ assets/js/your-code.js
@@ -0,0 +1,50 @@
+export function getAllDogs() {
+    // Your code here
+    return fetch('/dogs');
+}
+
+export function getDogNumberTwo() {
+    // Your code here
+    return fetch('/dogs/2');
+}
+
+export function postNewDog() {
+    // Your code here
+    return fetch("/dogs", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded"
+            },
+            body: new URLSearchParams({
+                'name': "paul",
+                'age': '20'
+            })
+        }
+    );
+}
+
+export function postNewDogV2(name, age) {
+     // Your code here
+     return fetch("/dogs", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded"
+        },
+        body: new URLSearchParams({
+            'name': `${name}`,
+            'age': `${age}`
+        })
+    }
+);
+}
+
+export function deleteDog(id) {
+    // Your code here
+    return fetch(`/dogs/${id}/delete`, {
+        method: "POST",
+        headers: {
+            "AUTH": "ckyut5wau0000jyv5bsrud90y"
+        },
+        }
+    );
+}
